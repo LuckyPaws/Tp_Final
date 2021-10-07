1d390756b790b5b37c0bc71631bfd50e5bf357bd
Lucky <linkot2015@gmail.com>
2021-10-07 17:20:11 +0200

    Liste utilisateur + liste articles

---
+++ src/Controller/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditType;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+/**
+ * @Route("/user")
+ * @IsGranted("ROLE_ADMIN")
+ */
+class UserController extends AbstractController
+{
+    /**
+     *@Route("/utilisateur", name="user_index", methods={"GET"})
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     *@Route("/{id}", name="user_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     *@Route("/{id}/edit", name="user_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(EditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     *@Route("/{id}", name="user_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/EditType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur Lambda' => 'ROLE_USER',
+                    'Modérateur' => 'ROLE_MODO',
+                    'Admin' => 'ROLE_ADMIN',
+                    'Ecrivain' => 'ROLE_ECRIVAIN'
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
